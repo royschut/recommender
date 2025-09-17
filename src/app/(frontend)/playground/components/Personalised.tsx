ff6f6651e139f8fb38f73f87f100675907138a9c
Roy Schut <roy@videodock.com>
2025-09-17 04:05:41 +0200

    refactor: clean up

--- src/app/(frontend)/playground/components/Personalised.tsx
+++
@@ -1,5 +0,0 @@
-const Personalised = () => {
-  return <div className="space-y-6">{'Personalised'}</div>
-}
-
-export default Personalised
--- src/app/(frontend)/playground/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-// UI Components for Playground
-export { default as Button } from './Button'
-export { default as Input } from './Input'
-export { default as Card } from './Card'
-export { default as Badge } from './Badge'
-export { default as SearchBox } from './SearchBox'
-export { default as Snackbar } from './Snackbar'
